9dd59c55a813287e70e7974b894238d8a8cfdb4e
Mark <pal.mark28@gmail.com>
2019-11-24 13:03:58 +0100

    Create first order low-pass and high-pass filters

---
+++ STM32Project/Inc/hpf1.h
@@ -0,0 +1,19 @@
+#ifndef _HPF1_H
+#define _HPF1_H	1
+
+
+//High pass filter in the form of
+//
+//G = (b0 + b1*z^-1)/(1 + a1*z^-1)
+
+typedef struct _HPF1 {
+
+	float lastY, lastU;
+	const float b0, b1, a1;
+
+} HPF1;
+
+float makeReadingHPF1(LPF1* lpf, float data);
+
+
+#endif
