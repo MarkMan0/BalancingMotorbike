094315de2238f5186aa69268701dc59554035be7
Mark <pal.mark28@gmail.com>
2020-01-07 16:35:58 +0100

    Refactoring

--- STM32Project/Src/FWSpdTracker.c
+++ STM32Project/Src/FWSpdTracker.c
@@ -6,8 +6,10 @@ volatile SpdTracker FWspeed = {0};
 
 void initTracker() {
 
-	FWspeed.lastCnt = LL_TIM_GetAutoReload(TIM_ENC) / 2;
-	FWspeed.ts = 1.0/50.0;	//updated in balance ISR
+	FWspeed.lastCnt = LL_TIM_GetAutoReload(TIM_ENC) / 2;	//start counting from the middle of possible values
+
+	FWspeed.ts = 1.0/50.0;		//to calculate speed
+	LL_TIM_EnableIT_UPDATE(TIM_SERVO);  //for velocity calculation
 
 	LL_TIM_SetCounter(TIM_ENC, FWspeed.lastCnt);
 	LL_TIM_ClearFlag_UPDATE(TIM_ENC);
--- STM32Project/Src/MovementController.c
+++ STM32Project/Src/MovementController.c
@@ -12,7 +12,6 @@ volatile MovementControl MC = {0};	//global variable initialization
 
 
 void initServo() {
-	LL_TIM_EnableIT_UPDATE(TIM_SERVO);
 	LL_TIM_OC_SetCompareCH1(TIM_SERVO, SERVO_CENTER);
 	LL_TIM_CC_EnableChannel(TIM_SERVO, LL_TIM_CHANNEL_CH1);
 	LL_TIM_EnableAllOutputs(TIM_SERVO);
