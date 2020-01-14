b651e81be18910ff89acbdb244f5ce75f2090f49
Mark <pal.mark28@gmail.com>
2020-01-14 14:50:38 +0100

    set counter to known value

--- STM32Project/Src/EncoderHandler.c
+++ STM32Project/Src/EncoderHandler.c
@@ -6,15 +6,14 @@ volatile SpdTracker FWspeed = {0};
 
 void initEncoder() {
 
-	FWspeed.lastCnt = LL_TIM_GetAutoReload(TIM_ENC) / 2;	//start counting from the middle of possible values
-
+	FWspeed.lastCnt = LL_TIM_GetAutoReload(TIM_ENC) / 2;
 	FWspeed.ts = 1.0/50.0;		//to calculate speed
-	LL_TIM_EnableIT_UPDATE(TIM_SERVO);  //for velocity calculation
 
-	LL_TIM_SetCounter(TIM_ENC, FWspeed.lastCnt);
+	LL_TIM_SetCounter(TIM2, FWspeed.lastCnt);
 	LL_TIM_ClearFlag_UPDATE(TIM_ENC);
 	LL_TIM_EnableIT_UPDATE(TIM_ENC);
 	LL_TIM_EnableCounter(TIM_ENC);
 
+	LL_TIM_EnableIT_UPDATE(TIM_SERVO);  //for velocity calculation
 
 }
