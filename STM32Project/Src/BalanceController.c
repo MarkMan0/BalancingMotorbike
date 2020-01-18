91a93d64abaac02f311bd77b1c45b72d1bbaa6c1
Mark <pal.mark28@gmail.com>
2020-01-18 19:29:46 +0100

    Fix time step

--- STM32Project/Src/BalanceController.c
+++ STM32Project/Src/BalanceController.c
@@ -5,6 +5,6 @@ volatile BalanceContParams BCParams = {
 		-8, 0.0, 0.0, //kp, ki, kd
 		0.0, //last I
 		0.0, //last Error
-		1.0/2000, 	//time step
+		1.0/1000, 	//time step
 		-1, 1,		//output min, max
 		0.00 };		//deadzone
