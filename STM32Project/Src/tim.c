c245eb704c16aec380075a8185f2d6494e0d7fbe
Mark <pal.mark28@gmail.com>
2020-01-20 22:59:11 +0100

    fix rear wheel pwm frequency

--- STM32Project/Src/tim.c
+++ STM32Project/Src/tim.c
@@ -156,9 +156,9 @@ void MX_TIM3_Init(void)
   /* Peripheral clock enable */
   LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_TIM3);
 
-  TIM_InitStruct.Prescaler = 160;
+  TIM_InitStruct.Prescaler = 159;
   TIM_InitStruct.CounterMode = LL_TIM_COUNTERMODE_UP;
-  TIM_InitStruct.Autoreload = 100;
+  TIM_InitStruct.Autoreload = 99;
   TIM_InitStruct.ClockDivision = LL_TIM_CLOCKDIVISION_DIV1;
   LL_TIM_Init(TIM3, &TIM_InitStruct);
   LL_TIM_EnableARRPreload(TIM3);
@@ -209,7 +209,7 @@ void MX_TIM6_Init(void)
   TIM_InitStruct.Autoreload = 399;
   LL_TIM_Init(TIM6, &TIM_InitStruct);
   LL_TIM_EnableARRPreload(TIM6);
-  LL_TIM_SetTriggerOutput(TIM6, LL_TIM_TRGO_UPDATE);
+  LL_TIM_SetTriggerOutput(TIM6, LL_TIM_TRGO_RESET);
   LL_TIM_DisableMasterSlaveMode(TIM6);
 
 }
